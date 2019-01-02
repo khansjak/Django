ba43246baab96b0ccaff2c16ae40a6a801decf85
Javed <khans.jak@gmail.com>
2019-01-03 00:34:57 +0530

    Blog v2.0

--- blog/migrations/0001_initial.py
+++ blog/migrations/0001_initial.py
@@ -3,7 +3,7 @@
 from django.conf import settings
 from django.db import migrations, models
 import django.db.models.deletion
-import django.utils.timezone
+import django.utils.timezone 
 
 
 class Migration(migrations.Migration):
